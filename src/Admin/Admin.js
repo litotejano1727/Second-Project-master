7641df89dae117db5ea786ab4c85e4fd52be23a8
Romy Guibelondo <112987484+RomyGuibelondo@users.noreply.github.com>
2022-12-09 07:52:55 +0800

    add admin file

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import TopBar from './TopBar'
+import './Admin.css'
 
 function Admin() {
   return (
-    <div>Admin</div>
+    <div>
+      <TopBar />
+    </div>
   )
 }
 
---
+++ src/Admin/TopBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TopBar() {
+  return (
+    <div className='topbar'>
+        <div className="topbarWrapper">
+            <div className="topLeft">
+                <span className="logo">Admin</span>
+            </div>
+            <div className="topRight">right</div>
+        </div>
+    </div>
+  )
+}
+
+export default TopBar
