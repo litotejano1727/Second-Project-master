b56db2031d1d77ac09a603f31f02ac8e9ed1a05d
Angelito Tejano <litotejano1727@gmail.com>
2022-12-08 11:44:27 +0800

    Corrected Typos

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <>
       <header className='header'>
         <div className='container d_flex'>
-          <div className='catgrories d_flex'>
+          <div className='categories d_flex'>
             <span class='fa-solid fa-border-all'></span>
             <h4>
               Categories <i className='fa fa-chevron-down'></i>
