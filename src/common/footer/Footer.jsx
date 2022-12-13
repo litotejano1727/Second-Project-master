111bd42acdd856b3fe80573ef7885ee7097ee5bb
unknown <romyguibelondo@gmail.com>
2022-12-13 11:00:31 +0800

    remove button

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -9,18 +9,7 @@ const Footer = () => {
           <div className='box'>
             <h1>Online Shop</h1>
             <p>"Our computer is a powerful and reliable device that is perfect for all your computing needs. With a fast processor, plenty of storage, and a sharp display, this computer will help you stay productive and entertained wherever you are. Whether you're browsing the web, working on a project, or playing your favorite games, this computer has the speed and performance you need. Plus, with a sleek design and slim profile, it will look great on any desk or in any backpack. Shop now and get the best computer for your needs."</p>
-            <div className='icon d_flex'>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-google-play'></i>
-                <span>Google Play</span>
-              </div>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-app-store-ios'></i>
-                <span>App Store</span>
-              </div>
-            </div>
           </div>
-
           <div className='box'>
             <h2>About Us</h2>
             <ul>
