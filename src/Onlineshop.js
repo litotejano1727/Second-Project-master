c14ce0c9c16d76f7fa3099f60a99ef6cd32fb64d
Angelito Tejano <litotejano1727@gmail.com>
2023-02-12 16:46:20 +0800

    Fix routing of login and singup

--- src/Onlineshop.js
+++ src/Onlineshop.js
@@ -8,96 +8,96 @@ import Footer from "./common/footer/Footer";
 import Sdata from "./components/shops/Sdata";
 import axios from "axios";
 import Details from "./components/productpage/Details";
+import SignIn from "./pages/signinandlogin/signin";
 import { Breadcrumb } from "react-bootstrap";
-import Auth from "./Auth";
-import Admin from "./Admin/Admin";
+
 import "./App.css";
 import ProductList from "./Product";
-
+import SignUp from "./pages/signinandlogin/signup";
 
 function Onlineshop() {
-  // API testing from here
-  const [apiResponse, setApiResponse] = useState(null);
+    // API testing from here
+    const [apiResponse, setApiResponse] = useState(null);
 
-  useEffect(() => {
-      axios
-          .get("http://localhost:9000")
-          .then((response) => {
-              setApiResponse(response.apiResponse);
-          })
-          .catch((error) => {
-              console.error(error);
-          });
-  }, []);
-  // to here
+    useEffect(() => {
+        axios
+            .get("http://localhost:9000")
+            .then((response) => {
+                setApiResponse(response.apiResponse);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+    // to here
 
-  const { productItems } = Data;
-  const { shopItems } = Sdata;
-  const [CartItem, setCartItem] = useState([]);
-  const addToCart = (product) => {
-      const productExit = CartItem.find((item) => item.id === product.id);
-      if (productExit) {
-          setCartItem(
-              CartItem.map((item) =>
-                  item.id === product.id
-                      ? { ...productExit, qty: productExit.qty + 1 }
-                      : item
-              )
-          );
-      } else {
-          setCartItem([...CartItem, { ...product, qty: 1 }]);
-      }
-  };
-  const decreaseQty = (product) => {
-      const productExit = CartItem.find((item) => item.id === product.id);
-      if (productExit.qty === 1) {
-          setCartItem(CartItem.filter((item) => item.id !== product.id));
-      } else {
-          setCartItem(
-              CartItem.map((item) =>
-                  item.id === product.id
-                      ? { ...productExit, qty: productExit.qty - 1 }
-                      : item
-              )
-          );
-      }
-  };
+    const { productItems } = Data;
+    const { shopItems } = Sdata;
+    const [CartItem, setCartItem] = useState([]);
+    const addToCart = (product) => {
+        const productExit = CartItem.find((item) => item.id === product.id);
+        if (productExit) {
+            setCartItem(
+                CartItem.map((item) =>
+                    item.id === product.id
+                        ? { ...productExit, qty: productExit.qty + 1 }
+                        : item
+                )
+            );
+        } else {
+            setCartItem([...CartItem, { ...product, qty: 1 }]);
+        }
+    };
+    const decreaseQty = (product) => {
+        const productExit = CartItem.find((item) => item.id === product.id);
+        if (productExit.qty === 1) {
+            setCartItem(CartItem.filter((item) => item.id !== product.id));
+        } else {
+            setCartItem(
+                CartItem.map((item) =>
+                    item.id === product.id
+                        ? { ...productExit, qty: productExit.qty - 1 }
+                        : item
+                )
+            );
+        }
+    };
 
-  return (
-      <>
-          <Header CartItem={CartItem} />
-          <Routes>
-              <Route
-                  path="/"
-                  element={
-                      <Pages
-                          productItems={productItems}
-                          addToCart={addToCart}
-                          shopItems={shopItems}
-                      />
-                  }
-              />
+    return (
+        <>
+            <Header CartItem={CartItem} />
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <Pages
+                            productItems={productItems}
+                            addToCart={addToCart}
+                            shopItems={shopItems}
+                        />
+                    }
+                />
 
-              <Route path="/details" element={<Details />} />
-              <Route
-                  path="/cart"
-                  element={
-                      <Cart
-                          CartItem={CartItem}
-                          addToCart={addToCart}
-                          decreaseQty={decreaseQty}
-                      />
-                  }
-              />
-              {/* <Route path="/" element={<App />} /> */}
-              <Route path="/productlist" element={<ProductList />} />
+                <Route path="/details" element={<Details />} />
+                <Route
+                    path="/cart"
+                    element={
+                        <Cart
+                            CartItem={CartItem}
+                            addToCart={addToCart}
+                            decreaseQty={decreaseQty}
+                        />
+                    }
+                />
+                {/* <Route path="/" element={<App />} /> */}
+                <Route path="/productlist" element={<ProductList />} />
 
-              <Route path="/login" element={<Auth />} />
-          </Routes>
-          <Footer />
-      </>
-  );
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/signin" element={<SignIn />} />
+            </Routes>
+            <Footer />
+        </>
+    );
 }
 
-
-export default Onlineshop
+export default Onlineshop;
--- src/pages/signinandlogin/signin.js
+++ src/pages/signinandlogin/signin.js
@@ -1,92 +1,95 @@
-import React from 'react';
-import {useFormik} from "formik";
+import React from "react";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import "./style.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function SignUp() {
+export default function SignIn() {
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+            confirmPassword: "",
+        },
+        validationSchema: Yup.object({
+            email: Yup.string()
+                .email("invalid Email Address")
+                .required("Required"),
+            password: Yup.string()
+                .password.max(12, "must be 12 characters or less")
+                .min(6, "minimum of 6 characters")
+                .required("Required"),
+            confirmPassword: Yup.string()
+                .oneOf([Yup.ref("password"), null], "Password must match")
+                .required("Required"),
+        }),
 
-const formik = useFormik(
-{
-initialValues:{
-    email:"",
-    password:"",
-    confirmPassword: "",
-},
-validationSchema: Yup.object({
-email: Yup.string().email("invalid Email Address").required("Required"),
-password: Yup.string().password.max(12, "must be 12 characters or less").min(6, "minimum of 6 characters").required("Required"),
-confirmPassword:Yup.string().oneOf([Yup.ref('password'),null], 'Password must match').required('Required')
-}
-
-),
-
-
- onSubmit: values => {
-    fetch('/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(values),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-
-})
-   return (
-    <div className='Auth-form-container'>
-    <form onSubmit={formik.handleSubmit} className="Auth-form">
-    <div className="Auth-form-content">
-    <h3 className="Auth-form-title">Sign-In</h3>
-    <div className="text-center">
-                            Not registered yey? {" "}
-                            <Link
-                                className="link-primary"
-                                to="./signup.js"
-                            >
-                                Sign-Up
-                            </Link>
-                        </div>
-    <div className='input-containerThree'>
-            <input
-            id="email"
-            name="email"
-            type="text"
-placeholder="Email"
-onChange={formik.handleChange}
-onBlur={formik.handleBlur}
-value={formik.values.email}
-/>
-{formik.touched.email && formik.errors.email ? <p>{formik.errors.email}</p> : null}
+        onSubmit: (values) => {
+            fetch("/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(values),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        },
+    });
+    return (
+        <div className="Auth-form-container">
+            <form onSubmit={formik.handleSubmit} className="Auth-form">
+                <div className="Auth-form-content">
+                    <h3 className="Auth-form-title">Sign-In</h3>
+                    <div className="text-center">
+                        Not registered yey?{" "}
+                        <Link className="link-primary" to="./signup.js">
+                            Sign-Up
+                        </Link>
+                    </div>
+                    <div className="input-containerThree">
+                        <input
+                            id="email"
+                            name="email"
+                            type="text"
+                            placeholder="Email"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+                        />
+                        {formik.touched.email && formik.errors.email ? (
+                            <p>{formik.errors.email}</p>
+                        ) : null}
+                    </div>
+                    <div className="input-containerFour">
+                        <input
+                            id="Password"
+                            name="Password"
+                            type="text"
+                            placeholder="confirmPassword"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.password}
+                        />
+                        {formik.touched.email && formik.errors.email ? (
+                            <p>{formik.errors.email}</p>
+                        ) : null}
+                    </div>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                    <p className="text-center mt-2">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </div>
+            </form>
         </div>
-        <div className='input-containerFour'>
-            <input
-            id="Password"
-            name="Password"
-            type="text"
-placeholder="confirmPassword"
-onChange={formik.handleChange}
-onBlur={formik.handleBlur}
-value={formik.values.password}
-/>
-{formik.touched.email && formik.errors.email ? <p>{formik.errors.email}</p> : null}
-</div>
-<div>
-<button type='submit'>Submit</button>
-</div>
-<p className="text-center mt-2">Forgot <a href="#">password?</a></p>
-</div>
-</form>
-</div>       
-  )
+    );
 }
-
 
 //try
