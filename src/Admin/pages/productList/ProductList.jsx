a5f40af09600c35bfeecce10991deeaae1c0f928
Angelito Tejano <litotejano1727@gmail.com>
2023-02-17 16:04:02 +0800

    Add Delete and Edit product in Admin

--- src/Admin/pages/productList/ProductList.jsx
+++ src/Admin/pages/productList/ProductList.jsx
@@ -1,68 +1,99 @@
-import { DataGrid } from "@mui/x-data-grid"
-import "./productList.css"
+import { DataGrid } from "@mui/x-data-grid";
+import "./productList.css";
 import { DeleteOutline } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { ProductRows } from "./DummyProduct";
-
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 function ProductList() {
-    const [data, setData] = useState(ProductRows)
+  const [productItems, setProductItems] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:9000/")
+      .then(response => {
+        setProductItems(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [productItems]);
+  const handleEditFromServer = async (id, updatedProduct) => {
+  try {
+    const response = await axios.put(`http://localhost:9000/products/${id}`, updatedProduct);
+    if (response.status === 200) {
+      setProductItems(productItems.map(item => item.id === id ? updatedProduct : item));
+    } else {
+      console.log(response);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    const handleDelete = (id) =>{
-        setData(data.filter(item=>item.id !== id))
-      };
 
-      const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'product', headerName: 'product', width: 200, renderCell: (params)=>{
-            return (
-              <div className='productListItem'>
-                <img className='productListImg' src={params.row.img} alt=""/>
-                {params.row.name}
-              </div>
-            )
-        } },
-        { field: 'stock', headerName: 'Stock', width: 200 },
-        {
-          field: 'price',
-          headerName: 'Price',
-          width: 145,
-        },
-        {
-          field: 'status',
-          headerName: 'Status',
-          width: 90,
-        },
-        {
-          field: 'action',
-          headerName: 'Action',
-          width: 160,
-          renderCell: (params)=>{
-            return(
-              <>
-              <Link to={"/Admin/Product/"+params.row.id}>
-                <button className='productListEdit'>Edit</button>
-              </Link>
-                <DeleteOutline className='productListDelete' onClick={()=>handleDelete(params.row.id)}/>
-              </>
-            )
-          }
-        },
-    
-      ];
+  const handleDeleteFromServer = async (id) => {
+    try {
+      const response = await axios.delete(`http://localhost:9000/products/delete/${id}`);
+      if (response.status === 200) {
+        setProductItems(productItems.filter(item => item.id !== id));
+      } else {
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'name', headerName: 'Product', width: 300, renderCell: (params) => {
+        return (
+          <div className='productListItem'>
+            <img className='productListImg' src={params.row.image} alt=""/>
+            {params.row.name}
+          </div>
+        );
+      }
+    },
+    { field: 'stocks', headerName: 'Stock', width: 100 },
+    {
+      field: 'price',
+      headerName: 'Price',
+      width: 145,
+    },
+    {
+      field: 'category',
+      headerName: 'Category',
+      width: 90,
+    },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 160,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={`/Admin/Product/${params.row.id}`}>
+              <button className='productListEdit'>Edit</button>
+            </Link>
+            <DeleteOutline className='productListDelete' onClick={() => handleDeleteFromServer(params.row.id)} />
+          </>
+        );
+      }
+    },
+  ];
+
   return (
     <div className="productList">
-        <DataGrid
-            disableSelectionOnClick
-            rows={data}
-            columns={columns}
-            pageSize={15}
-            rowsPerPageOptions={[5]}
-            checkboxSelection
-        />
+      <DataGrid
+        disableSelectionOnClick
+        rows={productItems}
+        columns={columns}
+        rowsPerPageOptions={[5, 10, 20]}
+        pageSize={20}
+        checkboxSelection
+      />
     </div>
-  )
+  );
 }
 
-export default ProductList
+export default ProductList;
