a9758ef580830fa019f02070cf269299786395a7
MARK RONIL MALAGA BAHILLO <115707775+macmac081416@users.noreply.github.com>
2023-02-18 07:43:51 +0800

    local data base

---
+++ src/pages/signinandlogin/Utils/database.jsx
@@ -0,0 +1,28 @@
+export const database = [
+    {
+        username: "user1@gmail.com",
+        password: "password1"
+    },
+
+    {
+        username: "user2@gmail.com",
+        password: "password2"
+    },
+
+    {
+        username: "user3@gmail.com",
+        password: "password3"
+    },
+
+    {
+        username: "placewell@gmail.com",
+        password: "mark"
+    },
+
+    {
+        username: "placewell01@gmail.com",
+        password: "mark"
+    }
+
+
+]
